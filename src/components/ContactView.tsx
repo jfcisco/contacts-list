5b6ebd2a82b6507c637aea4899802a047387624c
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-06 13:25:49 +0800

    Add initial details markup

--- src/components/ContactView.tsx
+++ src/components/ContactView.tsx
@@ -31,6 +31,13 @@ const ContactView = ({ modalRef, contact }: ContactViewProps): JSX.Element => {
     companyName
   } = contact;
 
+  /** Format given date according to US english date */
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString("en-US", {
+      dateStyle: "medium"
+    });
+  }
+
   return (
     <div ref={modalDivRef} className="modal fade" id="view-contact">
       <div className="modal-dialog modal-fullscreen-md-down" tabIndex={-1}>
@@ -39,15 +46,49 @@ const ContactView = ({ modalRef, contact }: ContactViewProps): JSX.Element => {
             <button type="button" className="btn-close" onClick={() => closeModal()}></button>
           </div>
           <div className="modal-body">
-            <h1>{`${lastName}, ${firstName} ${middleName}`}</h1>
-
+            <h1>Contact</h1>
+            <h2>{`${lastName}, ${firstName} ${middleName}`}</h2>
+            {/* TODO: Grid styling for dl */}
             <dl>
-              <dt>First Name:</dt>
+              <dt>First Name</dt>
               <dd>{firstName}</dd>
-              <dt>Middle Name:</dt>
+              
+              <dt>Middle Name</dt>
               <dd>{middleName}</dd>
-              <dt>Last Name:</dt>
+              
+              <dt>Last Name</dt>
               <dd>{lastName}</dd>
+              
+              <dt>Birthday</dt>
+              <dd>{formatDate(birthday)}</dd>
+
+              <dt>Gender</dt>
+              <dd>{gender ? gender : "Not specified"}</dd>
+              
+              <dt>Address</dt>
+              <dd>
+                <span>{address.addressLine}</span>
+                <span>{address.cityProvince}</span>
+                <span>{address.country}</span>
+              </dd>
+
+              <dt>Email Address</dt>
+              <dd>{emailAddress}</dd>
+
+              <dt>Contact Numbers:</dt>
+              <dd>
+                {/* TODO: Look up if below is legal HTML */}
+                <ul>
+                  {contactNumbers.map((contactNumber, i) => {
+                    // Assume that first contact number is primary
+                    // TODO: Style primary contact number
+                    return (<li key={i} className={(i === 0) ? "contact-primary" : ""}>{contactNumber}</li>);
+                  }) }
+                </ul>
+              </dd>
+
+              <dt>Company Name</dt>
+              <dd>{companyName ? companyName : "Not specified"}</dd>
             </dl>
           </div>
         </div>
