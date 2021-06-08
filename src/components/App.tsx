5548ecab97c92730e79b06cd4af844095f2465ec
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-08 20:19:47 +0800

    Remove unused code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,8 @@
 import ContactsList from './ContactsList';
 import ContactCreate from './ContactCreate';
 import { Pages, PageContextProvider, Page } from '../contexts/PageContext';
-import useContactsDelay from '../hooks/useContacts';
-import { useEffect, useState } from 'react';
 
 export default function App(): JSX.Element {
-  const { addContact } = useContactsDelay();
-
   return (
     <div className="container mt-2 mt-md-5">
       <h1 className="text-muted">My Contacts</h1>
