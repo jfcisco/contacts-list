db26a010ef643f3dec9a417d4fa0ba8e99e7aa13
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-09 19:46:32 +0800

    Refactor contacts hook to be more optimistic
    Changes are reflected on the UI and are rolled back when a problem is
    encountered.

--- src/hooks/useContacts.ts
+++ src/hooks/useContacts.ts
@@ -1,39 +1,64 @@
+import { useEffect, useState } from 'react';
 import { sampleContacts } from '../sampleContacts';
 import { Contact } from '../types/Contact';
 
 const DELAY_TIME = 700;
 const delay = () => new Promise((resolve) => setTimeout(resolve, DELAY_TIME));
 
-let contacts = sampleContacts;
-
-// Assumes that contacts are synchronous
-// TODO: Simulate a backend that is asynchronous for CRUD contacts
+/** Custom hook  for data operations. Simulates a backend that is asynchronous for CRUD contacts */ 
 export default function useContactsDelay() {
+  const [contacts, setContacts] = useState<Contact[]>([]);
+
+  useEffect(() => {
+    setContacts(sampleContacts);
+  }, []);
+
   function isExistingContact(testContact: Contact) {
     return contacts.find(c => c.id === testContact.id) !== undefined;
   }
 
   async function addContact(newContact: Contact) {
-    await delay();
-    contacts = [newContact, ...contacts];
+    const oldContacts = [...contacts];
+    setContacts(contacts => [newContact, ...contacts]);
+    
+    try {
+      await delay();
+    } catch (e) {
+      console.error(`An error occured! ${e}`) // Enhancement: replace with appropriate logger
+      setContacts(oldContacts); // rollback
+    }
+
     return newContact;
   }
 
   async function updateContact(updatedContact: Contact) {
-    await delay();
-    if (!isExistingContact(updatedContact)) throw new Error(`${updatedContact} is not an existing contact!`);
-
-    contacts = contacts.map(contact => (contact.id === updatedContact.id) ? updatedContact : contact);
+    const oldContacts = [...contacts];
+    setContacts(contacts => contacts.map(contact => (contact.id === updatedContact.id) ? updatedContact : contact));
+    
+    try {
+      await delay();
+      if (!isExistingContact(updatedContact)) throw new Error(`${updatedContact} is not an existing contact!`);
+    } catch (e) {
+      console.error(`An error occured! ${e}`) // Enhancement: replace with appropriate logger
+      setContacts(oldContacts);
+    }
+    
     return updatedContact;
   }
 
   async function deleteContact(contactToDelete: Contact) {
-    await delay();
-    if (!isExistingContact(contactToDelete)) throw new Error(`${contactToDelete} is not an existing contact!`);
+    const oldContacts = [...contacts];
+    setContacts(contacts => contacts.filter(c => c.id !== contactToDelete.id));
 
-    contacts = contacts.filter(c => c.id !== contactToDelete.id);
+    try {
+      await delay();
+      if (!isExistingContact(contactToDelete)) throw new Error(`${contactToDelete} is not an existing contact!`);
+    } catch (e) {
+      console.error(`An error occured! ${e}`) // replace with appropriate logger
+      setContacts(oldContacts);
+    }
   }
 
   // Using const assertion for custom hook: https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/hooks/#custom-hooks
-  return { contacts, addContact, updateContact };
+  return { contacts, addContact, updateContact, deleteContact };
 }
