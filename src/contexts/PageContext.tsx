1e5616957ce8b2d586c8e5ab50051ebce3f977a0
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-09 19:45:22 +0800

    Add (fake) progress bar

--- src/contexts/PageContext.tsx
+++ src/contexts/PageContext.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 /** Enumerates the pages of the application */
 export enum Pages {
@@ -11,13 +12,46 @@ type PageContextValue = {
   setCurrentPage: (page: Pages) => void;
 }
 
+/** Displays a Progress Bar stuck to the top of the viewport */
+const ProgressBar = ({ progress }: { progress: number }) => {
+  // Clamp progress to the range [0, 1]
+  const clampedProgressPercent = Math.max(0, Math.min(100, progress)).toString() + "%";
+
+  return ReactDOM.createPortal(
+    (<div 
+      className="progress w-100 mb-4 shadow-sm" 
+      style={
+        { 
+          height: "3px",
+          position: "fixed",
+          top: 0,
+        }}>
+      <div
+        className="progress-bar bg-info"
+        style={
+          {
+            width: clampedProgressPercent,
+            opacity: (progress / 100)
+          }} />
+      </div>
+    ), document.querySelector('html') as Element);
+}
+
 export const PageContext = React.createContext<PageContextValue>(null!);
 
 export function PageContextProvider({ children }: { children: React.ReactNode }): JSX.Element {
-  const [currentPage, setCurrentPage] = React.useState<Pages>(Pages.LIST);
+  const [currentPage, _setCurrentPage] = React.useState<Pages>(Pages.LIST);
+  const [progressState, setProgressState] = useState({ progress: 0, animate: "none" });
+
+  const PAGE_LOAD_DELAY_TIME = 600;
+  function setCurrentPage(page: Pages) {
+    setProgressState({ progress: 100, animate: "" })
+    setTimeout(() => { _setCurrentPage(page); setProgressState({ progress: 0, animate: "none !important" }); }, PAGE_LOAD_DELAY_TIME);
+  }
 
   return (
     <PageContext.Provider value={{ currentPage, setCurrentPage }}>
+      <ProgressBar progress={progressState.progress} />
       { children}
     </PageContext.Provider>
   )
