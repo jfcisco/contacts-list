745f404588ad2f27a2182d78deea0fbf00cb9b57
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-10 21:31:17 +0800

    Add validation error feedback

--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useFormContext } from '../../contexts/FormContext';
+import { FormValues } from '../../types/FormTypes';
 
 type TextInputProps = {
   name: string;
@@ -7,17 +7,26 @@ type TextInputProps = {
   label: string;
   classes?: string;
   required?: boolean;
+  type?: string;
 };
-// Default value for field class attribute 
-const DEFAULT_FIELD_CLASS = "form-control mb-2";
+
 /** Renders a controlled \<input type="text"\> */
-export function TextInput({ name, label, classes = DEFAULT_FIELD_CLASS, required }: TextInputProps) {
-  const { values, handleChange } = useFormContext();
+export function TextInput({ name, label, required, type }: TextInputProps) {
+  const { values, errors, handleChange, handleBlur, touched } = useFormContext<FormValues>();
+
+  const isInvalid = touched[name] && errors[name];
 
   return (
     <>
       <label htmlFor={name} className={`form-label ${(required === true ? "required-input" : "")}`}>{label}</label>
-      <input name={name} type="text" className={classes} required={required} value={values[name]} onChange={handleChange} />
+      <input 
+        name={name} 
+        type={type}
+        className={`form-control ${isInvalid ? "is-invalid" : ""}`} 
+        value={values[name]} 
+        onChange={handleChange} 
+        onBlur={handleBlur} />
+      { touched[name] && errors[name] && <div className="text-danger mb-2">{errors[name]}</div> }
     </>
   );
 }
