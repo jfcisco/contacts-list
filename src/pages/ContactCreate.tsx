879cb5fe31ad7fd18355e4bd6b25863672471c95
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-11 23:25:01 +0800

    Add email validation code

--- src/pages/ContactCreate.tsx
+++ src/pages/ContactCreate.tsx
@@ -4,17 +4,20 @@ import { Contact, Gender } from '../types/Contact';
 import { TextInput, BirthdayInput, GenderSelect, ContactNumbersInput, Form } from '../components/form';
 import { FormErrors, FormValues } from '../types/FormTypes';
 
+/** Checks if the given value is null or whitespace */
 export const isNullOrWhitespace = (value: string | null | undefined): boolean => {
-  if (value == null) return true; // loose compare is intentional
+  if (value == null) return true; // loose compare is intentional for checking null or undefined
   return (value.trim() === "");
 }
 
+/** Returns true if given value is a valid email. */
 export const isValidEmail = (value: string) => {
-  // TODO: Not yet implemented
-  return true;
+  // Format pattern is based  https://en.wikipedia.org/wiki/Email_address#Syntax
+  const validEmailFormat = /^([a-z]|[A-Z]|\d|\.(?!\.)|[!#$%&'*+-/=?^_`{|}~]){0,64}@([a-z]|[A-Z]|\d|-|\.){0,63}$/;
+  return validEmailFormat.test(value);
 }
 
-export interface CreateFormValues extends FormValues {
+interface CreateFormValues extends FormValues {
   firstName: string,
   middleName: string,
   lastName: string,
