7c79b14d59b8763199272b7a259891a0c14c5b14
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-11 22:31:54 +0800

    Reuse TextInput for BirthdayInput component

--- src/components/form/BirthdayInput.tsx
+++ src/components/form/BirthdayInput.tsx
@@ -1,14 +1,7 @@
-import { useFormContext } from '../../contexts/FormContext';
-import { FormValues } from '../../types/FormTypes';
+import { TextInput } from '.';
+import { FieldProps} from '../../types/FormTypes';
 
 /** Renders a controlled \<input type="date"\> to taken in a Birthday */
-export function BirthdayInput() {
-  const { values, handleChange } = useFormContext<FormValues>();
-
-  return (
-    <>
-      <label htmlFor="birthday" className="form-label required-input">Birthday</label>
-      <input name="birthday" type="date" className="form-control mb-2" value={values["birthday"]} required onChange={handleChange} />
-    </>
-  );
+export function BirthdayInput({ name }: FieldProps) {
+  return <TextInput name={name} type="date" required label="Birthday" />;
 }
