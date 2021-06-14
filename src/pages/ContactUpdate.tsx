d58aafbd289ef5f584e38be48927becd97fa29f1
Jasper Francisco <jasper.francisco@gmail.com>
2021-06-14 20:44:29 +0800

    Add logic to handle no contact scenario
    An error will be shown if no contact was passed in as a parameter for
    the ContactUpdate page.

--- src/pages/ContactUpdate.tsx
+++ src/pages/ContactUpdate.tsx
@@ -1,3 +1,30 @@
-export default function ContactUpdate() {
-  return (<p>Hello, world!</p>);
+import { Contact } from "../types/Contact";
+import { Page, PageContext } from "../contexts/PageContext";
+import { useContext } from "react";
+
+type ContactUpdateProps = {
+  updateContact: (c: Contact) => void;
+}
+
+export default function ContactUpdate({ updateContact }: ContactUpdateProps) {
+  const { payload: contact, setCurrentPage } = useContext(PageContext);
+
+  if (!contact) return <ContactUpdateErrorMessage />;
+
+  return (
+    <div>
+      <pre>{JSON.stringify(contact)}</pre>
+      <button onClick={() => setCurrentPage(Page.LIST)}>Go back!!!</button>
+    </div>
+  );
+}
+
+function ContactUpdateErrorMessage() {
+  const { setCurrentPage } = useContext(PageContext);
+  return (
+    <div className="text-center">
+      <p className="text-danger">Error while updating contact! No contact was selected for updating.</p>
+      <button onClick={() => setCurrentPage(Page.LIST)}>Go back</button>
+    </div>
+  );
 }
